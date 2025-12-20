acfd0d2fa6da0d31378fa9e41067a5b1f276822f
Rogerio Shimizu <roja@bunker79.com>
2025-12-20 20:29:59 -0300

    fix: fix js imports for Fabric and be more strict on the spec file

--- src/SpallaPlayerViewNativeComponent.ts
+++ src/SpallaPlayerViewNativeComponent.ts
@@ -1,10 +1,12 @@
-import {
-  codegenNativeComponent,
-  type ViewProps,
-  type CodegenTypes,
-  type HostComponent,
-  codegenNativeCommands,
-} from 'react-native';
+import type { ViewProps, CodegenTypes, HostComponent } from 'react-native';
+
+// NOTE: codegenNativeComponent MUST be imported from RN internals for Fabric.
+// Do not change this import to `from 'react-native'`.
+/* eslint-disable @react-native/no-deep-imports */
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+
+/* eslint-disable @react-native/no-deep-imports */
+import codegenNativeCommands from 'react-native/Libraries/Utilities/codegenNativeCommands';
 
 // Define a single comprehensive event payload interface
 interface PlayerEventPayload {
@@ -30,21 +32,21 @@ export interface NativeProps extends ViewProps {
 }
 
 export interface NativeCommands {
-  play: (viewRef: React.ElementRef<HostComponent<any>>) => void;
-  pause: (viewRef: React.ElementRef<HostComponent<any>>) => void;
+  play: (viewRef: React.ElementRef<HostComponent<NativeProps>>) => void;
+  pause: (viewRef: React.ElementRef<HostComponent<NativeProps>>) => void;
   seekTo: (
-    viewRef: React.ElementRef<HostComponent<any>>,
+    viewRef: React.ElementRef<HostComponent<NativeProps>>,
     time: CodegenTypes.Double
   ) => void;
   selectSubtitle: (
-    viewRef: React.ElementRef<HostComponent<any>>,
+    viewRef: React.ElementRef<HostComponent<NativeProps>>,
     subtitle: string | null
   ) => void;
   selectPlaybackRate: (
-    viewRef: React.ElementRef<HostComponent<any>>,
+    viewRef: React.ElementRef<HostComponent<NativeProps>>,
     rate: CodegenTypes.Float
   ) => void;
-  unmount: (viewRef: React.ElementRef<HostComponent<any>>) => void;
+  unmount: (viewRef: React.ElementRef<HostComponent<NativeProps>>) => void;
 }
 
 export const Commands: NativeCommands = codegenNativeCommands<NativeCommands>({
